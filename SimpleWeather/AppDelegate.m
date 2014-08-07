2d415c7b4c8a39ee696e50e63eccc1caa5003665
luminsense <wulianglumin@foxmail.com>
2014-08-08 00:54:43 +0800

    Setting up basic views

--- SimpleWeather/AppDelegate.m
+++ SimpleWeather/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "WXViewController.h"
+#import <TSMessage.h>
 
 @interface AppDelegate ()
             
@@ -18,9 +20,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    
+    self.window.rootViewController = [[WXViewController alloc] init];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    [TSMessage setDefaultViewController:self.window.rootViewController];
+    
     return YES;
 }
 
---
+++ SimpleWeather/WXViewController.h
@@ -0,0 +1,14 @@
+//
+//  WXViewController.h
+//  SimpleWeather
+//
+//  Created by Lumi on 14-8-7.
+//  Copyright (c) 2014年 LumiNg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXViewController : UIViewController
+<UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate>
+
+@end
