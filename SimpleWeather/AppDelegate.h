8933c153b8023a5670ed39d81779320d33b5fe82
luminsense <wulianglumin@foxmail.com>
2014-08-07 22:56:13 +0800

    initial commit
    project created

---
+++ SimpleWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by Lumi on 14-8-7.
+//  Copyright (c) 2014年 LumiNg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
